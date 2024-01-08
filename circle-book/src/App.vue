77de16c89506f0a78c99aa3db1eb3e705ca47cfa
Charlotte Lau <tweety.tinglau@gmail.com>
2024-01-08 21:28:43 +1300

    Banner added
    - Can handle with logo or no logo
    - shopping cart on the right

--- circle-book/src/App.vue
+++ circle-book/src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Banner from '@/components/Banner.vue'
+
+const goToCart = () =>{
+  console.log('goToCart..');
+}
 </script>
 
 <template>
   <header>
+    <Banner 
+      logo="/src/assets/icons/logo.png" 
+      title="Circle Book Shop" 
+      :goToCart="goToCart"/> 
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
---
+++ circle-book/src/components/Banner.vue
@@ -0,0 +1,73 @@
+<script setup>
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiCart, mdiBookSearch } from '@mdi/js'
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'Title'
+    },
+    logo: {
+        type: String,
+        default: ''
+    },
+    totalBooks: {
+        type: Number,
+        default: 0
+    },
+    goToCart: {
+        type: Function
+    }
+})
+</script>
+
+<template>
+<div class="container d-flex bg pa-2">
+    <div class="left">
+        <div v-if="logo != ''" class="logo"><img :src="logo"/></div>
+        <div class="text-h4 banner">{{ title }}</div>
+    </div>
+    <div class="icon pr-2">        
+        <div @click="goToCart"><svg-icon type="mdi" :path="mdiCart"></svg-icon>{{ totalBooks }}</div>
+        
+    </div>
+</div>
+</template>
+
+<style scoped>
+.container {
+    justify-content: space-between;
+}
+.left {
+    display: flex;
+    align-items: center;
+}
+.banner {
+    font-size: 1.9rem !important;
+}
+.logo {
+    padding-right: 10px;
+}
+.icon {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+}
+.icon > div {
+    display: flex;
+    padding-left: 5px;
+}
+
+@media (min-width: 1024px) {
+.icon > div {
+    padding-left: 10px;
+}
+}
+.banner {
+    font-size: 2.125rem !important;
+}
+
+.bg {
+    background-color: #81D4FA;
+}
+</style>
