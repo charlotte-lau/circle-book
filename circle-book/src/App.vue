8b38c21c174f6a766e9a5ddf73ab25666ae7e294
Charlotte Lau <tweety.tinglau@gmail.com>
2024-01-09 20:24:13 +1300

    purchase and home icon

--- circle-book/src/App.vue
+++ circle-book/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { useCartStore } from '@/stores/cart';
 import Banner from '@/components/Banner.vue'
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiHome } from '@mdi/js'
 
+const cartStore = useCartStore();
 const goToCart = () =>{
   console.log('goToCart..');
 }
@@ -12,10 +16,11 @@ const goToCart = () =>{
     <Banner 
       logo="/src/assets/icons/logo.png" 
       title="Circle Book Shop" 
+      :totalBooks="cartStore.count"
       :goToCart="goToCart"/> 
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/"><svg-icon type="mdi" :path="mdiHome"></svg-icon></RouterLink>
       </nav>
     </div>
   </header>
